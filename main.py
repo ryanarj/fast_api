db2f54ff8e63abc361ccf256e708c5b6dbe29470
ryanarj <ryanarj07@gmail.com>
2024-01-02 16:34:25 -0500

    update with flake8

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 from enum import Enum
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -7,8 +6,8 @@ app = FastAPI()
 
 
 class Category(Enum):
-    PLAYERS = 'players'
-    STAFF = 'staff'
+    PLAYERS = "players"
+    STAFF = "staff"
 
 
 class Player(BaseModel):
@@ -30,50 +29,28 @@ class Team(BaseModel):
 
 
 players = {
-    0: Player(
-        id=0,
-        name="John Starks",
-        number=3,
-        position="SG",
-        team="Knicks"
-    ),
-    1: Player(
-        id=1,
-        name="Patrick Ewing",
-        number=33,
-        position="C",
-        team="Knicks"
-    ),
-    2: Player(
-        id=2,
-        name="Charles Oakley",
-        number=34,
-        position="PF",
-        team="Knicks"
-    )
+    0: Player(id=0, name="John Starks", number=3, position="SG", team="Knicks"),
+    1: Player(id=1, name="Patrick Ewing", number=33, position="C", team="Knicks"),
+    2: Player(id=2, name="Charles Oakley", number=34, position="PF", team="Knicks"),
 }
 
 
-@app.get('/')
+@app.get("/")
 def index():
     return {"players": players}
 
 
-@app.get('/players/{player_id}')
+@app.get("/players/{player_id}")
 def query_player_id(player_id: int):
     if not players.get(player_id):
-        raise HTTPException(
-            status_code=404
-        )
+        raise HTTPException(status_code=404)
     return players.get(player_id)
 
 
-@app.post('/add_players')
+@app.post("/add_players")
 def query_player_id(player: Player):
-    print('Test')
+    print("Test")
     if player.id in players:
-        raise HTTPException(
-            status_code=400
-        )
+        raise HTTPException(status_code=400)
     players[player.id] = player
     return {"players": players}
--- test_server.py
+++ test_server.py
@@ -2,12 +2,17 @@ import requests
 
 from main import Player
 
-print(
-    requests.get("http://127.0.0.1:8000/players/1").json()
-)
+print(requests.get("http://127.0.0.1:8000/players/1").json())
 
 print(
-    requests.post("http://127.0.0.1:8000/add_players", json={
-        "id": 7, "name": "Carmelo Anthony", "number": 7, "position": 'PF', "team": 'Knicks'
-    }).json()
+    requests.post(
+        "http://127.0.0.1:8000/add_players",
+        json={
+            "id": 7,
+            "name": "Carmelo Anthony",
+            "number": 7,
+            "position": "PF",
+            "team": "Knicks",
+        },
+    ).json()
 )
