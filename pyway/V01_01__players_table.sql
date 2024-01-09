1018f8c3bdce43ac577870e4f48459181c725380
ryanarj <ryanarj07@gmail.com>
2024-01-09 11:32:48 -0500

    migrations works

--- pyway/V01_01__players_table.sql
+++ pyway/V01_01__players_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS players (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
   name TEXT,
   number INT,
   position INT,
-  team TEXT,
-)
+  team TEXT
+);
