0ae9d0f731f7ae2c15124f3290f7dae46edd7e26
ryanarj <ryanarj07@gmail.com>
2024-01-18 10:58:58 -0500

    update with classes

--- services/tests/test_players.py
+++ services/tests/test_players.py
@@ -1,15 +1,16 @@
-from routers.players.players import add_player
-from schema.models import PlayerBase
+import unittest
+
 from schema.tables import Player
-from services.players.players import commit_player, get_player_by_id
+from services.players.players import PlayerService
 
 
-def test_create_player():
-    player = Player(name="<NAME>", number=12, position="player", team="test")
-    player = commit_player(player)
-    player = player["player"]
-    test_player = get_player_by_id(player.id)
-    assert player.name == test_player.name
-    assert player.number == test_player.number
-    assert player.position == test_player.position
-    assert player.team == test_player.team
+class TestPlayerService(unittest.TestCase):
+    def test_create_and_get_player(self):
+        player = Player(name="<NAME>", number=12, position="player", team="test")
+        player = PlayerService.commit_player(player)
+        player = player["player"]
+        test_player = PlayerService.get_player_by_id(player.id)
+        self.assertEquals(player.name, test_player.name)
+        self.assertEquals(player.number, test_player.number)
+        self.assertEquals(player.position, test_player.position)
+        self.assertEquals(player.team, test_player.team)
