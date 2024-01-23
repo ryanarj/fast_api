f2000da0b8bb62d7efa53896182928aed0842650
ryanarj <ryanarj07@gmail.com>
2024-01-23 12:34:56 -0500

    update

---
+++ services/laminiLMM/lamini.py
@@ -0,0 +1,11 @@
+# import lamini
+# lamini.api_key = "463f53863e605253918a957926ef578cbc0864ca5e73acc8decb0999fc8b46fb"
+#
+# llm = lamini.Lamini("meta-llama/Llama-2-7b-chat-hf")
+# print(llm.generate("Will software engineers be replaced by AI?"))
+
+class LaminiService:
+
+    def get_service(self):
+        pass
+
